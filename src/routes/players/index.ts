a5a86ef4a228d4bc41b3d499e14f15904f2fd7a8
Astarose <baptiste.regnier62@gmail.com>
2024-04-11 11:43:21 +0200

    Ajoutons la logique métier - 1

---
+++ src/routes/players/index.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Find");
+});
+
+router.get("/:id", (req: Request, res: Response) => {
+  res.send("Get");
+});
+
+router.post("/", (req: Request, res: Response) => {
+  res.send("Create");
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+  res.send("Update");
+});
+
+router.delete("/:id", (req: Request, res: Response) => {
+  res.send("Delete");
+});
+
+export const playersRouter = router;
